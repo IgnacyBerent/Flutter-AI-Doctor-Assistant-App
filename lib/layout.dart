2d312ac4f72822f430c5bdb20033c057d005ad67
IgnacyBerent <2gb02ignac@gmail.com>
2025-11-30 01:46:41 +0100

    Initial commit

---
+++ lib/layout.dart
@@ -0,0 +1,16 @@
+import 'package:ai_doctor_assistant/speech.dart';
+import 'package:flutter/material.dart';
+
+class Layout extends StatelessWidget {
+  const Layout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Speech(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:ai_doctor_assistant/layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AI Doctor Assistant',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.cyan)),
+      home: const Layout(),
+    );
+  }
+}
